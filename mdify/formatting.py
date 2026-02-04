46b256c588c8fe969fb65f82a12587f4c3b16023
Ivan Shamray <ivan.shamray@exactprosystems.com>
2026-02-04 09:49:59 +0700

    fix: enhance Colorizer docstring and add ANSI style codes

--- mdify/formatting.py
+++ mdify/formatting.py
@@ -7,7 +7,32 @@ from typing import TextIO
 
 
 class Colorizer:
-    """ANSI color helper for terminal output."""
+    """ANSI color and style helper for terminal output."""
+
+    # ANSI color codes
+    RESET = "0"
+    BOLD = "1"
+    DIM = "2"
+    ITALIC = "3"
+    UNDERLINE = "4"
+    
+    # Colors
+    BLACK = "30"
+    RED = "31"
+    GREEN = "32"
+    YELLOW = "33"
+    BLUE = "34"
+    MAGENTA = "35"
+    CYAN = "36"
+    WHITE = "37"
+    BRIGHT_BLACK = "90"
+    BRIGHT_RED = "91"
+    BRIGHT_GREEN = "92"
+    BRIGHT_YELLOW = "93"
+    BRIGHT_BLUE = "94"
+    BRIGHT_MAGENTA = "95"
+    BRIGHT_CYAN = "96"
+    BRIGHT_WHITE = "97"
 
     def __init__(self, stream: TextIO) -> None:
         force_color = os.environ.get("FORCE_COLOR")
